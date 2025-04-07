ebde4877a6b33325814a8aaf920544593f27955e
Jonathan Rochkind <jonathan@dnil.net>
2025-04-07 16:39:03 -0400

    demo of immediate mode on application_controller, with allow_exempt working
    route /dummy_download is protected, but /dummy is excepted, since action == 'index'

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+   before_action do |controller|
+     BotChallengePage::BotChallengePageController.bot_challenge_enforce_filter(controller, immediate: true)
+   end
 end
--- spec/dummy/app/controllers/dummy_controller.rb
+++ spec/dummy/app/controllers/dummy_controller.rb
@@ -1,9 +1,4 @@
 class DummyController < ApplicationController
-  # normal one for index
-  before_action(only: :index) { |controller| BotChallengePage::BotChallengePageController.bot_challenge_enforce_filter(controller) }
-
-  # immediate one for download please
-  before_action(only: :download) { |controller| BotChallengePage::BotChallengePageController.bot_challenge_enforce_filter(controller, immediate: true) }
 
   def index
     render plain: "rendered action dummy#index"
