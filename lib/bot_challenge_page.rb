f2a23da5d290daa5d913cdfbe178772e916b2925
Jonathan Rochkind <jonathan@dnil.net>
2025-08-04 12:22:52 -0400

    config convenience

--- lib/bot_challenge_page.rb
+++ lib/bot_challenge_page.rb
@@ -4,4 +4,14 @@ require "bot_challenge_page/config"
 
 module BotChallengePage
   mattr_reader :config, default: ::BotChallengePage::Config.new
+
+  # Just a convenience to allow
+  #
+  #     BotChallengePage.configure do |config|
+  #       config.foo = "bar"
+  #     end
+  #
+  def self.configure
+    yield config
+  end
 end
